8a285a1ec536e5954dacd733155668b36f3b9ec4
JuanSHerrera1006 <jsebastianhg1006@gmail.com>
2023-05-23 01:21:33 -0500

    feat(utils): Add functions to verify path and remove items

--- utils/path.py
+++ utils/path.py
@@ -11,3 +11,36 @@ def get_root_dir() -> str:
     CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
     ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
     return ROOT_DIR
+
+def path_exists(path):
+    """Check if the path exists
+
+    Parameters
+    ----------
+        path: str
+            The path that will be verify
+
+    Returns
+    -------
+        ans: bool
+            The results if the path exists
+    """
+    ROOT_DIR = get_root_dir()
+    DEST_DIR = os.path.abspath(os.path.join(ROOT_DIR, path))
+    return os.path.exists(DEST_DIR)
+
+def remove_file(path):
+    """Remove a file of a path
+
+    Parameters
+    ----------
+        path: str
+            Path of the file to delete
+
+    Returns
+    -------
+        None
+    """
+    ROOT_DIR = get_root_dir()
+    DEST_DIR = os.path.abspath(os.path.join(ROOT_DIR, path))
+    return os.remove(DEST_DIR)
