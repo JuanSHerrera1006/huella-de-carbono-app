85f13ef17ce0a19ad31f99fd3b347d25deb4add3
JuanSHerrera1006 <jsebastianhg1006@gmail.com>
2023-05-23 01:20:59 -0500

    feat(utils): Add new function to show images

--- utils/images.py
+++ utils/images.py
@@ -1,6 +1,8 @@
 from tkinter.filedialog import askopenfilename
 from utils.path import get_root_dir
 from PIL import Image 
+import matplotlib.pyplot as plt
+import matplotlib.image as matplotlib_image
 import constants.constants as constants
 import os
 import io
@@ -17,8 +19,15 @@ def image2blob() -> tuple[str, bytes]:
     """
     img_path = ""
     # Ask to the file explorer
-    while img_path == "":
+    while True:
         img_path = askopenfilename(filetypes=constants.IMAGES_FILETYPES)
+        if img_path != "":
+            break
+        
+        print("Debes de seleccionar un archivo de imagen tipo (.jpg, .png, .jpeg)")
+        input("Presiona cualquier tecla para continuar...")
+        print("")
+
     image = Image.open(img_path)
     img_format = image.format or 'JPEG'
     # Get the extension of the image
@@ -40,7 +49,7 @@ def blob2image(image_bytes: bytes) -> Image.Image:
     Returns
     -------
         image: Image
-            The image type PIL.Image.Image
+            The image type PIL.Image.ImageDB Browser for SQLite
     """
     stream = io.BytesIO(image_bytes)
     return Image.open(stream)
@@ -63,3 +72,25 @@ def save_image(img: Image.Image, image_name: str) -> None:
     DEST_PATH = ".\\assets\\images\\" + image_name
     IMG_PATH = os.path.abspath(os.path.join(ROOT_DIR, DEST_PATH))
     img.save(IMG_PATH)
+
+def show_image(image_name: str) -> None:
+    """Show the image and its name 
+
+    Parameters
+    ----------
+        image_name: str
+            The name of the image. The name should have the extension on the finish.
+
+    Returns
+    -------
+        None
+    """
+    ROOT_DIR = get_root_dir()
+    DEST_PATH = ".\\assets\\images\\" + image_name
+    IMG_PATH = os.path.abspath(os.path.join(ROOT_DIR, DEST_PATH))
+    # Plot image
+    fig = matplotlib_image.imread(IMG_PATH)
+    plt.imshow(fig)
+    plt.axis('off')
+    plt.title(image_name)
+    plt.show()
